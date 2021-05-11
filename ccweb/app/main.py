4595534a25b2c9b73faea3616f42db925bbc857d
chriskok <pherkok@gmail.com>
2021-05-11 11:27:30 -0500

    Restructuring

--- ccweb/app/main.py
+++ ccweb/app/main.py
@@ -5,8 +5,11 @@ from fastapi.staticfiles import StaticFiles
 from starlette.responses import FileResponse 
 
 app = FastAPI()
-app.mount("/public", StaticFiles(directory="app/public"), name="public")
+# app.mount("/public", StaticFiles(directory="app/public"), name="public")
+app.mount("/assets", StaticFiles(directory="app/assets"), name="assets")
+app.mount("/css", StaticFiles(directory="app/css"), name="css")
+app.mount("/js", StaticFiles(directory="app/js"), name="js")
 
 @app.get("/")
 async def read_index():
-    return FileResponse('app/public/index.html')
+    return FileResponse('app/index.html')
