a7724da6944a84504331cec59eace5ab8ddbe05c
chriskok <pherkok@gmail.com>
2021-05-15 09:02:47 -0500

    Updated SSL certs for new domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(chronic-coder.com www.chronic-coder.com)
+domains=(chronic-coder.com www.chronic-coder.com chriskok.site www.chriskok.site)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
